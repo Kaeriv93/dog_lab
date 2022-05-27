97a9e8e9482e39dec52c36b7fd7fe43008393204
kaeriv93 <vsan93@yahoo.com>
2022-05-27 14:44:26 -0500

    successfully added an admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=1000)
+    type = models.CharField(max_length=200)
+    bio = models.TextField(max_length=1000)
+    cool_dog = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Dog
 
 class Home(TemplateView):
     template_name = 'home.html'
+    
+class About(TemplateView):
+    template_name = 'about.html'
+
+class DogList(TemplateView):
+    template_name = 'dog_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['dogs'] = Dog.objects.all()
+        return context
 
 # Create your views here.
